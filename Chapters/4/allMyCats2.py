8b853c15360b710a2bf858bcfbfd6b1b2f6893ac
Floyd Zamarripa <Zamarripa232@gmail.com>
2024-08-27 23:10:12 -0400

    List exmaple usage

---
+++ Chapters/4/allMyCats2.py
@@ -0,0 +1,17 @@
+# allMyCats1
+# Automate the Boring Stuff
+# Chapter 4
+
+catNames = []
+
+while True:
+    print('Enter the name of a cat ' + str(len(catNames) + 1) + ' or enter nothing to stop.):')
+    name = input()
+    if name == '':
+        break;
+    catNames = catNames + [name] # list concatenation
+
+print('The cat names are:')
+
+for name in catNames:
+    print(' ' + name)
