d093d0ff9a8a30f2ab91a5963e22d8ee6827225a
Floyd Zamarripa <Zamarripa232@gmail.com>
2024-10-01 01:54:24 -0400

    Finished remaining project, charGrid.py for chapter 4

---
+++ Chapters/4/charGrid.py
@@ -0,0 +1,20 @@
+# charGrid.py
+# Automate the Boring Stuff
+# Chapter 4
+# Practice Project 3
+# use a pre-supplied nested list, print values rotated 90 degrees
+
+GRID = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+for x in range(len(GRID[1])):
+    for y in GRID:
+        print(str(y[x]), end='')
+    print()
