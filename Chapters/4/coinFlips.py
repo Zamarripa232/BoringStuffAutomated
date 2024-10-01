39d8d03b4007862c8cc0375f9d0bba75c2d16704
Floyd Zamarripa <Zamarripa232@gmail.com>
2024-10-01 01:03:31 -0400

    complete coinFlips.py, now more functional.

--- Chapters/4/coinFlips.py
+++ Chapters/4/coinFlips.py
@@ -3,22 +3,48 @@
 # Chapter 4
 # Practice Project 2
 # flip coins, create a list of h/t, 
-# check list for streak(s) of 6
-# goal: 10,000 coin flips
+# check list for streak 6 after 100 flips
+# goal: repeat 10,000 times, print % of experiments that had a streak of 6
 
 import random
 
-numberOfStreaks = 0
-flips = []
+FLIPCOUNT = 100
+TOTALEXPERIMENTS = 10000
 
-for experimentNumber in range(1):
-    # create list of h/t values.
-    flipResult = random.randint(0,1)
-    print('Doing a coin flip')
-    print('The result was ' + str(flipResult))
-    print('adding flip to list')
-    print('going back to flip again')
-    # check for 6 streak.
-    print('I\'m checking for a streak of 6...') # look at six at a time?
+totalFlips = FLIPCOUNT * TOTALEXPERIMENTS
+numberOfStreaks = 0
+
+def flipCoin():
+    """Returns the result of a coin flip, either 1 for heads, 0 for tails."""
+    return random.randint(0,1)
+
+def createFlipList(amtOfFlips):
+    """Returns a list of coin flips"""
+    tmpFlips = []
+    for i in range(amtOfFlips):
+        flipResult = flipCoin()
+        tmpFlips.append(flipResult)
+    return tmpFlips
+
+def checkStreak(headsTails, flipList):
+    """Returns 1 if a streak is found"""
+    streak = 0
+    for i in flipList:
+        if i == headsTails:
+            streak += 1
+            if streak == 6:
+                return 1
+        else:
+            streak = 0
+    return 0
+
+for experimentNumber in range(TOTALEXPERIMENTS):
+    flips = createFlipList(FLIPCOUNT)
+    if checkStreak(1, flips) > 0:
+        numberOfStreaks += 1
+        continue
+    elif checkStreak(0, flips) > 0:
+        numberOfStreaks += 1
+        continue
 
 print('Chance of streak: %s%%' % (numberOfStreaks / 100))
