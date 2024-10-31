8fc36226216b16c409466563217b4242b971a9ac
Floyd Zamarripa <Zamarripa232@gmail.com>
2024-10-31 16:56:12 -0400

    end of day, dictionaries example.

--- Chapters/5/birthdays.py
+++ Chapters/5/birthdays.py
@@ -1,1 +1,20 @@
-# a script about birthdays I guess about dictionaries or lists
+# birthdays.py
+# Automate the Boring Stuff
+# Chapter 5
+# Data about friends birthdays
+
+birthdays = {'Alice': 'Apr 1', 'Bob': 'Dec 12', 'Carol': 'Mar 4'}
+while True:
+    print()
+    print('Enter a name: (blank to quit)')
+    name = input()
+    if name == '':
+        break
+    if name in birthdays:
+        print(birthdays[name] + ' is the birthday of ' + name)
+    else:
+        print('I do not have any birthday information for ' + name)
+        print('What is their birthday?')
+        bday = input()
+        birthdays[name] = bday
+        print('Birthday database updated')
